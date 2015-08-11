410b6116cc6def00c0f029f414b8552ba89369ee
Christopher Gaens <christopher.gaens@harmonygroup.eu>
2015-08-11 15:35:56 +0200

    init

---
+++ TodoSPA/App/Scripts/app.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/ToGoList", {
+        controller: "toGoListCtrl",
+        templateUrl: "/App/Views/ToGoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    var endpoints = {
+
+        // Map the location of a request to an API to a the identifier of the associated resource
+        "togo-harmony.azurewebsites.net":
+            "https://systeembeheerbeharmonygroup.onmicrosoft.com/ToGoAPI",
+    };
+
+    adalProvider.init(
+        {
+            instance: 'https://login.microsoftonline.com/',
+            tenant: 'systeembeheerbeharmonygroup.onmicrosoft.com',
+            clientId: 'dfbad70b-2280-474a-9417-38e0a5d7026e',
+            extraQueryParameter: 'nux=1',
+            endpoints: endpoints,
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.  
+            // Also, token acquisition for the To Go API will fail in IE when running on localhost, due to IE security restrictions.
+        },
+        $httpProvider
+        );
+   
+}]);
